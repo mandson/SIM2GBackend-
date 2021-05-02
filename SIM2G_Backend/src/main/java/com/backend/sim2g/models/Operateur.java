9c9bff59aec24fa56d2c7680e181e30251e689bf
Mande Hamed <mandson1er@gmail.com>
2021-05-02 14:32:25 +0000

    model operateur creer

---
+++ SIM2G_Backend/src/main/java/com/backend/sim2g/models/Operateur.java
@@ -0,0 +1,14 @@
+package com.backend.sim2g.models;
+
+public class Operateur {
+
+	public Operateur() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
