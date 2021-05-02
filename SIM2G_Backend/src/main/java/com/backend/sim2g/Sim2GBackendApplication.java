82530aa1b471260fbc876313d9cbe0ac204fdd06
Mande Hamed <mandson1er@gmail.com>
2021-05-02 14:08:53 +0000

    initialisation

---
+++ SIM2G_Backend/src/main/java/com/backend/sim2g/Sim2GBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.sim2g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sim2GBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sim2GBackendApplication.class, args);
+	}
+
+}
---
+++ SIM2G_Backend/src/test/java/com/backend/sim2g/Sim2GBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sim2g;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sim2GBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
